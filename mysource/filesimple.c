5f7e05b056ef5807e883842100962958b0c15f40
Sua Lee <21700525@handong.edu>
2018-05-15 18:20:47 +0900

    example2

--- mysource/filesimple.c
+++ mysource/filesimple.c
@@ -7,10 +7,30 @@
  * A small example of jsmn parsing when JSON structure is known and number of
  * tokens is predictable.
  */
+char *readJSONFile() {
+	char oneline[250];
 
-static const char *JSON_STRING =
-	"{\"user\": \"johndoe\", \"admin\": false, \"uid\": 1000,\n  "
-	"\"groups\": [\"users\", \"wheel\", \"audio\", \"video\"]}";
+	char *js = (char *)malloc(sizeof(oneline));
+	int count=0;
+	FILE *fp;
+
+	fp = fopen("./mysource/data.json", "r");
+	if(fp == NULL){
+		printf("파일이 없음");
+		return NULL;
+	}
+
+	while(1){
+		fgets(oneline, sizeof(oneline),fp);
+		if(feof(fp)) break;
+
+		count += strlen(oneline);
+		js = (char *)realloc(js, count+1);
+		strcat(js, oneline);
+	}
+	fclose(fp);
+	return js;
+}
 
 static int jsoneq(const char *json, jsmntok_t *tok, const char *s) {
 	if (tok->type == JSMN_STRING && (int) strlen(s) == tok->end - tok->start &&
@@ -25,34 +45,32 @@ int main() {
 	int r;
 	jsmn_parser p;
 	jsmntok_t t[128]; /* We expect no more than 128 tokens */
-
+	char *JSON_STRING = readJSONFile();
+	printf("JSON_STRING : %s",JSON_STRING);
+/*
 	jsmn_init(&p);
+
 	r = jsmn_parse(&p, JSON_STRING, strlen(JSON_STRING), t, sizeof(t)/sizeof(t[0]));
 	if (r < 0) {
 		printf("Failed to parse JSON: %d\n", r);
 		return 1;
 	}
 
-	/* Assume the top-level element is an object */
 	if (r < 1 || t[0].type != JSMN_OBJECT) {
 		printf("Object expected\n");
 		return 1;
 	}
 
-	/* Loop over all keys of the root object */
 	for (i = 1; i < r; i++) {
 		if (jsoneq(JSON_STRING, &t[i], "user") == 0) {
-			/* We may use strndup() to fetch string value */
 			printf("- User: %.*s\n", t[i+1].end-t[i+1].start,
 					JSON_STRING + t[i+1].start);
 			i++;
 		} else if (jsoneq(JSON_STRING, &t[i], "admin") == 0) {
-			/* We may additionally check if the value is either "true" or "false" */
 			printf("- Admin: %.*s\n", t[i+1].end-t[i+1].start,
 					JSON_STRING + t[i+1].start);
 			i++;
 		} else if (jsoneq(JSON_STRING, &t[i], "uid") == 0) {
-			/* We may want to do strtol() here to get numeric value */
 			printf("- UID: %.*s\n", t[i+1].end-t[i+1].start,
 					JSON_STRING + t[i+1].start);
 			i++;
@@ -60,7 +78,7 @@ int main() {
 			int j;
 			printf("- Groups:\n");
 			if (t[i+1].type != JSMN_ARRAY) {
-				continue; /* We expect groups to be an array of strings */
+				continue;
 			}
 			for (j = 0; j < t[i+1].size; j++) {
 				jsmntok_t *g = &t[i+j+2];
@@ -73,4 +91,5 @@ int main() {
 		}
 	}
 	return EXIT_SUCCESS;
+*/
 }
